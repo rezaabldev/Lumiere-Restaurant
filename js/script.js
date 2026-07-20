7cd54bdfc991381b41e362804fb883a62fdb7ac8
Reza <reza.aboali.dev@gmail.com>
2026-07-20 14:50:50 +0430

    Add responsive hamburger menu

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const menuToggle = document.querySelector(".menu-toggle");
+
+const navbar = document.querySelector(".navbar");
+
+menuToggle.addEventListener("click", () => {
+
+    navbar.classList.toggle("active");
+
+    menuToggle.classList.toggle("active"); }
+
+);
+
